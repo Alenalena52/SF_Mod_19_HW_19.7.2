19922da32522aa14432bdcaf9de98d4cff95f606
Alenalena52 <118267598+Alenalena52@users.noreply.github.com>
2023-02-22 13:35:21 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 valid_email = 'forteslesson@gmail.com'
 valid_password = 'qwerty'
 
-invalid_email = 'zimadojd@yandex.ru'
-invalid_password = 'zxcvb'
+invalid_email = 'homepet@yandex.ru'
+invalid_password = 'frank'
