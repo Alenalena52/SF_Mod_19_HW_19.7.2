c6bd378ba2724c9a4641136b020a28554c350d0a
Alenalena52 <118267598+Alenalena52@users.noreply.github.com>
2023-03-01 18:07:42 +0300

    Update test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,8 +1,9 @@
-from api import PetFriends
+from app import PetFriends
 from settings import valid_email, valid_password, invalid_email, invalid_password
 import os
 
 pf = PetFriends()
+
 
 def test_get_api_key_valid_user(email=valid_email, password=valid_password):
     '''Проверяем что код статуса запроса 200 и в переменной result
@@ -10,6 +11,7 @@ def test_get_api_key_valid_user(email=valid_email, password=valid_password):
     status, result = pf.get_app_key(email, password)
     assert status == 200
     assert 'key' in result
+
 
 def test_get_all_pets_with_valid_key(filter=''):
     '''Проверяем что код статуса запроса 200 и список всех питомцев не пустой
@@ -21,7 +23,8 @@ def test_get_all_pets_with_valid_key(filter=''):
     assert status == 200
     assert len(result['pets']) > 0
 
-def test_add_pets_with_valid_data(name='Fedor', animal_type='cat', age='3', pet_photo='images/Fedor.jpg'):
+
+def test_add_pets_with_valid_data(name='Ben', animal_type='cat', age='1', pet_photo='images/Ben.jpg'):
     '''Проверяем что код статуса запроса 200 и что список с добавленными данными не пустой для этого
     в переменную pet_photo сохраняем путь к файлу фотографии питомца, сохраняем ключ в переменную api_key,
     проверяем статус ответа и что в ответе содержатся добавленные данные.
@@ -33,13 +36,14 @@ def test_add_pets_with_valid_data(name='Fedor', animal_type='cat', age='3', pet_
     assert status == 200
     assert result['name'] == name
 
+
 def test_delete_pet():
     '''Проверяем возможность удаления питомца'''
     _, api_key = pf.get_app_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(api_key, 'my_pets')
 
     if len(my_pets['pets']) == 0:
-        pf.add_new_pets(api_key, 'Murzik', 'cat', '5', 'images/Fedor.jpg')
+        pf.add_new_pets(api_key, 'Ben', 'cat', '1', 'images/Ben.jpg')
         _, my_pets = pf.get_list_of_pets(api_key, 'my_pets')
 
     pet_id = my_pets['pets'][0]['id']
@@ -49,6 +53,7 @@ def test_delete_pet():
 
     assert status == 200
     assert pet_id not in my_pets.values()
+
 
 def test_update_pet_info(name='из', animal_type='измененный', age='5'):
     '''Проверяем возможность изменения данных питомца'''
@@ -62,7 +67,8 @@ def test_update_pet_info(name='из', animal_type='измененный', age='5
     else:
         raise Exception("Питомцы отсутствуют")
 
-def test_add_pets_with_valid_data_without_photo(name='МурзикБезФото', animal_type='кот', age='6'):
+
+def test_add_pets_with_valid_data_without_photo(name='ПитомецБезФото', animal_type='кот', age='3'):
     '''Проверяем возможность добавления нового питомца без фото'''
     _, api_key = pf.get_app_key(valid_email, valid_password)
     status, result = pf.add_new_pet_without_photo(api_key, name, animal_type, age)
@@ -70,7 +76,8 @@ def test_add_pets_with_valid_data_without_photo(name='МурзикБезФото
     assert status == 200
     assert result['name'] == name
 
-def test_add_photo_at_pet(pet_photo='images/German.jpg'):
+
+def test_add_photo_at_pet(pet_photo='images/Cezar.jpg'):
     '''Проверяем возможность добавления новой фотографии питомца'''
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
 
@@ -87,7 +94,8 @@ def test_add_photo_at_pet(pet_photo='images/German.jpg'):
     else:
         raise Exception("Питомцы отсутствуют")
 
-def test_add_pet_negative_age_number(name='Fedor', animal_type='cat', age='-3', pet_photo='images/Fedor.jpg'):
+
+def test_add_pet_negative_age_number(name='Ben', animal_type='cat', age='-3', pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавление питомца с отрицательным числом в переменной age.
     Тест не будет пройден если питомец будет добавлен на сайт с отрицательным числом в поле возраст.
      '''
@@ -98,9 +106,10 @@ def test_add_pet_negative_age_number(name='Fedor', animal_type='cat', age='-3', 
 
     assert age not in result['age'], 'Питомец добавлен на сайт с отрицательным числом в поле возраст'
 
-def test_add_pet_with_four_digit_age_number(name='Fedor', animal_type='cat', age='1234', pet_photo='images/Fedor.jpg'):
+
+def test_add_pet_with_four_digit_age_number(name='Ben', animal_type='cat', age='5263', pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавление питомца с числом более трех знаков в переменной age.
-    Тест не будет пройден ели питомец будет добавлен на сайт с числом превышающим три знака в поле возраст.'''
+    Тест не будет пройден если питомец будет добавлен на сайт с числом, превышающим три знака в поле возраст.'''
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
 
     _, api_key = pf.get_app_key(valid_email, valid_password)
@@ -109,7 +118,8 @@ def test_add_pet_with_four_digit_age_number(name='Fedor', animal_type='cat', age
 
     assert len(number) < 4, 'Питомец добавлен на сайт с числом привышающим 3 знака в поле возраст'
 
-def test_add_pet_with_empty_value_in_variable_name(name='', animal_type='cat', age='2', pet_photo='images/Fedor.jpg'):
+
+def test_add_pet_with_empty_value_in_variable_name(name='', animal_type='cat', age='2', pet_photo='images/Ben.jpg'):
     '''Проверяем возможность добавления питомца с пустым значением в переменной name
     Тест не будет пройден если питомец будет добавлен на сайт с пустым значением в поле "имя"'''
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
@@ -119,11 +129,12 @@ def test_add_pet_with_empty_value_in_variable_name(name='', animal_type='cat', a
     assert status == 200
     assert result['name'] != '', 'Питомец добавлен на сайт с пустым значением в имени'
 
-def test_add_pet_with_a_lot_of_words_in_variable_name(animal_type='cat', age='2', pet_photo='images/Fedor.jpg'):
+
+def test_add_pet_with_a_lot_of_words_in_variable_name(animal_type='cat', age='3', pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавления питомца имя которого превышает 10 слов
     Тест не будет пройден если питомец будет добавлен на сайт с именем состоящим из более 10 слов'''
 
-    name = 'Пабло Диего Хозе Франциско де Паула Хуан Непомукено Криспин Криспиано де ла Сантисима Тринидад Руиз и Пикассо'
+    name = 'Кручу верчу запутать хочу и снова кручу верчу запутать хочу вот'
 
     _, api_key = pf.get_app_key(valid_email, valid_password)
     status, result = pf.add_new_pets(api_key, name, animal_type, age, pet_photo)
@@ -134,11 +145,12 @@ def test_add_pet_with_a_lot_of_words_in_variable_name(animal_type='cat', age='2'
     assert status == 200
     assert word_count < 10, 'Питомец добавлен с именем больше 10 слов'
 
-def test_add_pet_with_special_characters_in_variable_animal_type(name='Fedor', age='3', pet_photo='images/Fedor.jpg'):
+
+def test_add_pet_with_special_characters_in_variable_animal_type(name='Ben', age='3', pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавление питомца с специальными символами вместо букв в переменной animal_type.
-    Тест не будет пройден если питомец будет добавлен на сайт с спец.символами вместо букв в поле порода.
+    Тест не будет пройден если питомец будет добавлен на сайт с спец символами вместо букв в поле Порода
     '''
-    animal_type = 'Cat%@'
+    animal_type = 'Cat&@'
     symbols = '#$%^&*{}|?/><=+_~@'
     symbol = []
 
@@ -151,11 +163,11 @@ def test_add_pet_with_special_characters_in_variable_animal_type(name='Fedor', a
     for i in symbols:
         if i in result['animal_type']:
             symbol.append(i)
-    assert symbol[0] not in result['animal_type'], 'Питомец добавлен с недопустимыми спец.символами'
+    assert symbol[0] not in result['animal_type'], 'Питомец добавлен с недопустимыми спец символами'
 
-    
 
-def test_add_pet_with_numbers_in_variable_animal_type(name='Fedor', animal_type='34562', age='3', pet_photo='images/Fedor.jpg'):
+def test_add_pet_with_numbers_in_variable_animal_type(name='Ben', animal_type='34562', age='5',
+                                                      pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавление питомца с цифрами вместо букв в переменной animal_type.
     Тест не будет пройден если питомец будет добавлен на сайт с цифрами вместо букв в поле порода.'''
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
@@ -167,11 +179,11 @@ def test_add_pet_with_numbers_in_variable_animal_type(name='Fedor', animal_type=
     assert animal_type not in result['animal_type'], 'Питомец добавлен на сайт с цифрами вместо букв в поле порода'
 
 
-def test_add_pet_with_a_lot_of_words_in_variable_animal_type(name='Fedor', age='2', pet_photo='images/Fedor.jpg'):
+def test_add_pet_with_a_lot_of_words_in_variable_animal_type(name='Ben', age='2', pet_photo='images/Ben.jpg'):
     '''Проверка с негативным сценарием. Добавления питомца название породы которого превышает 10 слов
     Тест не будет пройден если питомец будет добавлен на сайт с названием породы состоящим из более 10 слов'''
 
-    animal_type = 'артезиано нормандский бассет гриффон миттельшнауцер ньюфаундленд аргентинский дог помесь с бриар басерон'
+    animal_type = 'пудель стаф мопс йорк чеширский кот норвежская полосатая корги спаниэль вислоухий'
 
     _, api_key = pf.get_app_key(valid_email, valid_password)
     status, result = pf.add_new_pets(api_key, name, animal_type, age, pet_photo)
@@ -182,6 +194,7 @@ def test_add_pet_with_a_lot_of_words_in_variable_animal_type(name='Fedor', age='
     assert status == 200
     assert word_count < 10, 'Питомец добавлен с названием породы больше 10 слов'
 
+
 def test_get_api_key_with_wrong_password_and_correct_mail(email=valid_email, password=invalid_password):
     '''Проверяем запрос с невалидным паролем и с валидным емейлом.
     Проверяем нет ли ключа в ответе'''
@@ -189,11 +202,10 @@ def test_get_api_key_with_wrong_password_and_correct_mail(email=valid_email, pas
     assert status == 403
     assert 'key' not in result
 
+
 def test_get_api_key_with_wrong_email_and_correct_password(email=invalid_email, password=valid_password):
     '''Проверяем запрос с невалидным паролем и с валидным емейлом.
     Проверяем нет ли ключа в ответе'''
     status, result = pf.get_app_key(email, password)
     assert status == 403
     assert 'key' not in result
-
-#
